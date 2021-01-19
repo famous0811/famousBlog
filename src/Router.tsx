def5afbdfe41cf8638121279ed79d8463d0a8dcb
famous0811 <umh0811@naver.com>
2021-01-19 16:14:10 +0900

    up

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/home'
-import Mypage from './pages/mypage'
-import Portfolios from './pages/portfolios';
-import Writed from './pages/Guestbook';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Mypage from "./pages/mypage";
+import Portfolios from "./pages/portfolios";
+import Writed from "./pages/Guestbook";
 
-import Login from './pages/admin/login'
-import Writepage from './pages/admin/writeportfolio';
-import SignUp from './pages/admin/signup';
+import Login from "./pages/admin/login";
+import Writepage from "./pages/admin/writeportfolio";
+import SignUp from "./pages/admin/signup";
 
-import writemypages from './pages/admin/writemypage';
-import Reviseportfolio from './pages/admin/Reviseportfolio';
-export default(
-    <Router basename="/famousBlog">
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/mypage" component={Mypage}/>
-            <Route exact path="/guestbook" component={Writed}/>
-            <Route
-				path={['/portfolios/:id', '/portfolios']}
-				exact
-				component={Portfolios}
-			/>
-            <Route exact path="/adminlogin" component={Login}/>
-            <Route exact path="/adminsignup" component={SignUp}/>
-            <Route exact path="/adminwritemypages" component={writemypages}/>
-            <Route exact path="/adminReviseportfolio/:id" component={Reviseportfolio}/>
-            <Route exact path="/write" component={Writepage}/>
-        </Switch>
-    </Router>
+import writemypages from "./pages/admin/writemypage";
+import Reviseportfolio from "./pages/admin/Reviseportfolio";
+export default (
+  <BrowserRouter basename="/famousBlog">
+    <Route exact path="/" component={Home} />
+    <Route exact path="/mypage" component={Mypage} />
+    <Route exact path="/guestbook" component={Writed} />
+    <Route
+      path={["/portfolios/:id", "/portfolios"]}
+      exact
+      component={Portfolios}
+    />
+    <Route exact path="/adminlogin" component={Login} />
+    <Route exact path="/adminsignup" component={SignUp} />
+    <Route exact path="/adminwritemypages" component={writemypages} />
+    <Route exact path="/adminReviseportfolio/:id" component={Reviseportfolio} />
+    <Route exact path="/write" component={Writepage} />
+  </BrowserRouter>
 );
